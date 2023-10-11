6fee7f0687ebdf9c4ccdb4aa5aea4f5613b69ff3
Martin Albert <martin.albert.187@gmail.com>
2023-10-11 17:59:05 +0200

    Add flag for limiting organizations

--- cmd/baton-dockerhub/config.go
+++ cmd/baton-dockerhub/config.go
@@ -12,14 +12,19 @@ import (
 type config struct {
 	cli.BaseConfig `mapstructure:",squash"` // Puts the base config options in the same place as the connector options
 
-	Username string `mapstructure:"username"`
-	Password string `mapstructure:"password"`
+	Username string   `mapstructure:"username"`
+	Password string   `mapstructure:"password"`
+	Orgs     []string `mapstructure:"orgs"`
 }
 
 // validateConfig is run after the configuration is loaded, and should return an error if it isn't valid.
 func validateConfig(ctx context.Context, cfg *config) error {
 	if cfg.Username == "" || cfg.Password == "" {
 		return fmt.Errorf("username and password must be provided")
+	}
+
+	if len(cfg.Orgs) == 0 {
+		return fmt.Errorf("at least one organization must be provided")
 	}
 
 	return nil
@@ -29,4 +34,5 @@ func validateConfig(ctx context.Context, cfg *config) error {
 func cmdFlags(cmd *cobra.Command) {
 	cmd.PersistentFlags().String("username", "", "The DockerHub username used to connect to the DockerHub API. ($BATON_USERNAME)")
 	cmd.PersistentFlags().String("password", "", "The DockerHub password used to connect to the DockerHub API. ($BATON_PASSWORD)")
+	cmd.PersistentFlags().StringSlice("orgs", nil, "Limit syncing to specific organizations by providing organization slugs. ($BATON_ORGS)")
 }
